fb90b6be7ce1217e81b510f0a2aab8bf7a10c91d
Crystal Parker <vertfromage@gmail.com>
2023-11-01 13:30:08 -0300

    Started working on gun script, put it on tower for easier testing, same script should be on enemies guns. Target will be decided by separate targetting scripts.

---
+++ CSCI4168Project/Assets/Scripts/GunScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Note: refered to this StackOverflow question to get the gun rotation right: https://stackoverflow.com/a/56570217
+
+public class gunScript : MonoBehaviour
+{
+    /* PUBLIC VARIABLES */
+    public Transform target;
+    public float rotationSpeed;
+    public float projectileSpeed; // bullet will need it's own script...
+
+
+    /* PRIVATE VARIABLES */
+    private Transform gunTransform;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gunTransform = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gunTransform != null && target != null)
+        {
+            // Have the gun point at the target
+            gunTransform.LookAt(target);
+            // make sure it's rotated at the right angle
+            transform.Rotate(Vector3.right * 90);
+
+
+            // Create a projectile and start it 
+        }
+    }
+}
