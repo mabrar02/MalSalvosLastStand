022ea80ae9138295407e20235b7be366c4407a9f
mabrar02 <mahdeen02@gmail.com>
2023-11-16 03:16:57 -0400

    added outline, started wrench and upgrade scripts

--- CSCI4168Project/Assets/WrenchScript.cs
+++ CSCI4168Project/Assets/WrenchScript.cs
@@ -5,14 +5,36 @@ using UnityEngine;
 public class WrenchScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private float wrenchRange;
+    private GameObject lastHighlightedObject;
+
+    private Camera cam;
+    private RaycastHit hit;
+    private LayerMask turretLayer;
+
+  
     void Start()
     {
+        cam = Camera.main;
+        turretLayer = LayerMask.GetMask("Tower");
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, wrenchRange, turretLayer)) {
+            if(hit.collider.gameObject !=  lastHighlightedObject) {
+                Debug.Log("TURRET DETECTED: " + hit.transform.name);
+                lastHighlightedObject = hit.collider.gameObject;
+            }
+        }
+        else {
+            lastHighlightedObject = null;
+        }
+
         
+
     }
+
 }
