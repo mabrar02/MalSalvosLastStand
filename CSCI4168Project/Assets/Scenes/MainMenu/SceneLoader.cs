5cf0a504f35f3ebd70c10c87c4cbd6c705578c0e
JonahJF <148268748+JonahJF@users.noreply.github.com>
2023-11-30 21:19:57 -0400

    Added main menu and victory screen

---
+++ CSCI4168Project/Assets/Scenes/MainMenu/SceneLoader.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadForestScene(){
+        SceneManager.LoadScene("MidscopeUpdate");
+    }
+
+    public void LoadSceneSelecter(){
+        SceneManager.LoadScene("SceneSelect");
+    }
+
+    public void LoadIceScene(){
+        SceneManager.LoadScene("BluePlanet");
+    }
+
+    public void LoadFireScene(){
+        SceneManager.LoadScene("RedPlanet");
+    }
+
+    public void LoadMenuScreen(){
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadNextScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
